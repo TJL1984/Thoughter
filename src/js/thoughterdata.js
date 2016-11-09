59828acaece2226ebabae8ab3fa12373e90b0348
TJ Liwanag <timothyjliwanag@gmail.com>
2016-11-09 15:38:58 -0500

    edited the folder names, now i need to make the views switch

---
+++ src/js/thoughterdata.js
@@ -0,0 +1,15 @@
+(function() {
+  'use strict';
+
+    window.addEventListener('hashchange', function(eventObj){
+      console.log('hash changed', window.location);
+
+      var listOfElems = document.querySelectorAll('main'); // hiding stuff
+      for (var i=0, )
+
+      var theElement = document.querySelector(window.location.hash)
+        theElement.style.display = 'block'
+
+    })
+
+}());
