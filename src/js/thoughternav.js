8ed6ec1a164bfba28476a42536b49106e8f2e25e
TJ Liwanag <timothyjliwanag@gmail.com>
2016-11-09 22:29:23 -0500

    needed a switcher for the views

---
+++ src/js/thoughternav.js
@@ -0,0 +1,10 @@
+(function() {
+  'use strict';
+  window.thoughter = window.thoughter || {};
+
+  $(window).on('hashchange', function viewChange(){
+    $('main').hide();
+    $(window.location.hash).show();
+  });
+
+}());
